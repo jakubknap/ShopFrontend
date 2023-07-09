537f16284c0d067733c24c9c90c36ed0abc7eb33
Jakub Knap <jknap40@gmail.com>
2023-07-09 22:35:47 +0200

    Dodanie obsługi kategorii

---
+++ src/app/modules/admin/admin-category/admin-category.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AdminConfirmDialogService } from '../admin-confirm-dialog.service';
+import { AdminCategoryNameDto } from '../common/dto/adminCategoryNameDto';
+import { AdminCategoryService } from './admin-category.service';
+import { MatTable } from '@angular/material/table';
+
+@Component({
+  selector: 'app-admin-category',
+  templateUrl: './admin-category.component.html',
+  styleUrls: ['./admin-category.component.scss']
+})
+export class AdminCategoryComponent implements OnInit {
+
+  displayedColumns: string[] = ["id", "name", "actions"];
+  data: Array<AdminCategoryNameDto> = [];
+  @ViewChild(MatTable) table !: MatTable<any>;
+
+  constructor(
+    private dialogService: AdminConfirmDialogService,
+    private adminCategoryService: AdminCategoryService) { }
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.adminCategoryService.getCategories().subscribe(categories => this.data = categories);
+  }
+
+  confirmDelete(element: AdminCategoryNameDto) {
+    this.dialogService.openConfirmDialog("Czy na pewno chcesz usunąć kategorię ?")
+      .afterClosed()
+      .subscribe(result => {
+        if (result) {
+          this.adminCategoryService.delete(element.id).subscribe(() => {
+            this.data.forEach((value, index) => {
+              if (element == value) {
+                this.data.splice(index, 1);
+                this.table.renderRows();
+              }
+            })
+          });
+        }
+      });
+  }
+}
