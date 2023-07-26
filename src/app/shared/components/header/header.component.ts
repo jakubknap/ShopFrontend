9e5244b71989ee69d988fdfff9ee1c1524ec0acb
Jakub Knap <jknap40@gmail.com>
2023-07-26 19:01:24 +0200

    Profil klienta

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import { HeaderService } from './header.service';
 import { CartIconService } from 'src/app/modules/common/service/cart-icon.service';
+import { JwtService } from 'src/app/modules/common/service/jwt.service';
+import { HeaderService } from './header.service';
 
 @Component({
   selector: 'app-header',
@@ -11,21 +12,24 @@ import { CartIconService } from 'src/app/modules/common/service/cart-icon.servic
 export class HeaderComponent implements OnInit {
   title = "Shop";
   cartProductCounter = "";
+  isLoggedIn = false;
 
   constructor(
     private cookieService: CookieService,
     private headerService: HeaderService,
-    private cartIconService: CartIconService
-    ) { }
+    private cartIconService: CartIconService,
+    private jwtService: JwtService
+  ) { }
 
   ngOnInit(): void {
+    this.isLoggedIn = this.jwtService.isLoggedIn();
     this.getCountProducts();
     this.cartIconService.subject
-      .subscribe(counter => this.cartProductCounter = String(counter > 0 ? counter:""));
+      .subscribe(counter => this.cartProductCounter = String(counter > 0 ? counter : ""));
   }
 
-  getCountProducts(){
+  getCountProducts() {
     this.headerService.getCountProducts(Number(this.cookieService.get("cartId")))
-      .subscribe(counter => this.cartProductCounter = String(counter > 0 ? counter:""));
+      .subscribe(counter => this.cartProductCounter = String(counter > 0 ? counter : ""));
   }
 }
